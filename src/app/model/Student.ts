c456b98383c44e4a288b7f2fb41bec0d45acc9f6
GayanathBuddhika <pbgaya1993@gmail.com>
2020-01-13 01:59:46 +0530

    create model class

---
+++ src/app/model/Student.ts
@@ -0,0 +1,12 @@
+import { Department } from './Department';
+
+export class Student{
+    id: string;
+    ai: number;
+    epNumber: string;
+    registrationNumber: string;
+    batch: string;
+    studentName: string;
+    degreeProYear: number;
+    department: Department;
+}
