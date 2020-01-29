09c06121cd4099a0216f6ed27d6d7cde7973812c
GayanathBuddhika <pbgaya1993@gmail.com>
2020-01-29 18:09:44 +0530

    create  lecture component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,10 @@ import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { DegreeProgramComponent } from './component/degree-program/degree-program.component';
 import { AddDegreeProgramComponent } from './component/degree-program/add-degree-program/add-degree-program.component';
 import { DegreeProgramListComponent } from './component/degree-program/degree-program-list/degree-program-list.component';
+import { TectureComponent } from './component/tecture/tecture.component';
+import { LectureComponent } from './component/lecture/lecture.component';
+import { AddLectureComponent } from './component/lecture/add-lecture/add-lecture.component';
+import { LectureListComponent } from './component/lecture/lecture-list/lecture-list.component';
 // import { StudentComponent } from './component/student/student.component';
 // import { StudentListComponent } from './component/student/student-list/student-list.component';
 // import { StudentAddComponent } from './component/student/student-add/student-add.component';
@@ -33,6 +37,10 @@ import { DegreeProgramListComponent } from './component/degree-program/degree-pr
     HeaderComponent,
     LeftNavTemplateComponent,
     NavigationComponent,
+    TectureComponent,
+    LectureComponent,
+    AddLectureComponent,
+    LectureListComponent,
    
  
    
