cb93e817032c1920e1bcd153c052cc7aa706f28c
GayanathBuddhika <pbgaya1993@gmail.com>
2020-03-30 04:05:20 +0530

    create result add form

--- src/app/component/result/add-result/add-result.component.ts
+++ src/app/component/result/add-result/add-result.component.ts
@@ -1,3 +1,7 @@
+import { StudentCourse } from './../../../model/StudentCourse';
+import { DegreeCourse } from 'app/model/DegreeCourse';
+import { FormGroup, FormBuilder, Validators, FormArray } from '@angular/forms';
+import { ResultService } from 'app/service/result.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,10 +10,124 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-result.component.scss']
 })
 export class AddResultComponent implements OnInit {
+  resultForm: FormGroup;
+  resultForm2: FormGroup;
+  batchs: any[] = [];
+  headers: any[];
+  degreeCourse: DegreeCourse[] = [];
+  studentCourseList: StudentCourse[] = [];
+  resultMarks: any[] = [];
+  viewTable: boolean = false;
+  constructor(
+    private resultService: ResultService,
+    private formBuilder: FormBuilder
 
-  constructor() { }
+  ) { }
 
   ngOnInit() {
+    this.getBatchs();
+    this.getSubjects();
+    this.getResultMark();
+
+    this.resultForm = this.formBuilder.group({
+      batch: ["", Validators.required],
+      degreeCourse: ["", Validators.required],
+
+    });
+
+    this.resultForm2 = this.formBuilder.group({
+      data: this.formBuilder.array([])
+    })
+
+    this.headers =
+      [
+        { field: 'no', header: 'No' },
+        { field: 'epNumber', header: 'Ep Number' },
+        { field: 'name', header: 'Name' },
+        { field: 'result', header: 'Result' },
+        // { field: 'edit', header: 'Edit' },
+        // { field: 'delete', header: 'Delete' }
+
+      ];
+
+
+
+  }
+
+  initItem(): FormGroup {
+    return this.formBuilder.group({
+
+      epNumber: ["", Validators.required],
+      name: ["", Validators.required],
+      mark: ["", Validators.required],
+    })
+  }
+
+  getBatchs() {
+    this.resultService.getbatchs().subscribe(data => {
+      console.log("batches data", data);
+      data.forEach(element => {
+        let newbatch = {
+          label: element,
+          value: element
+        }
+        this.batchs.push(newbatch);
+      });
+      console.log("new batches data", this.batchs);
+    }, err => {
+      console.log(err);
+    })
+  }
+
+  getSubjects() {
+    //   67a5f5a9-206e-42b6-9f4f-b2686f8be8a2
+    this.resultService.getCourse("67a5f5a9-206e-42b6-9f4f-b2686f8be8a2").subscribe(data => {
+      this.degreeCourse = data;
+      console.log(this.degreeCourse);
+    }, err => {
+      console.log(err);
+    })
+
+
+  }
+
+  findSutudent() {
+    console.log("select subject ", this.resultForm.value)
+    this.resultService.findStudents(this.resultForm.value.degreeCourse.course.id, this.resultForm.value.batch).subscribe(data => {
+      console.log("students", data);
+      this.studentCourseList = data;
+
+      if (data) {
+        // add form group to form array
+        this.studentCourseList.forEach(element => {
+          this.addrows();
+        })
+        // set value to rows 
+        const controlls = this.resultForm2.get('data') as FormArray;
+      
+        controlls.controls.forEach((element, i) => {
+          element.get('epNumber').patchValue(this.studentCourseList[i].student.epNumber);
+          element.get('name').patchValue(this.studentCourseList[i].student.studentName);
+        })
+        this.viewTable = true;
+      }
+
+
+    }, err => {
+      console.log(err);
+    })
+  }
+
+  getResultMark() {
+    this.resultMarks = this.resultService.result;
+    console.log("result Marks", this.resultMarks);
+  }
+  submit(data: any) {
+    console.log("table data",this.resultForm2.value);
+  }
+  addrows() {
+    const control = this.resultForm2.get('data') as FormArray;
+    control.push(this.initItem());
   }
 
 }
--- src/app/service/result.service.ts
+++ src/app/service/result.service.ts
@@ -1,6 +1,6 @@
 import { BehaviorSubject, Subject, Observable } from 'rxjs';
 import { Injectable } from '@angular/core';
-import { HttpClient} from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Result } from 'app/model/Result';
 
 @Injectable({
@@ -12,12 +12,77 @@ export class ResultService {
 
   public _addResultToList = new Subject<Result>();
   public _editResultToList = new Subject<Result>();
-  constructor(private http: HttpClient) { }
+ result = [
+   {
+     label: 'A+',
+     value: 'A+'
+   },
+   {
+    label: 'A',
+    value: 'A'
+  },
+  {
+    label: 'A-',
+    value: 'A-'
+  },
+  {
+    label: 'B+',
+    value: 'B+'
+  },
+  {
+    label: 'B',
+    value: 'B'
+  },
+  {
+    label: 'B-',
+    value: 'B-'
+  },
+  {
+    label: 'C+',
+    value: 'C+'
+  },
+  {
+    label: 'C',
+    value: 'C'
+  },
+  {
+    label: 'C-',
+    value: 'C-'
+  },
+  {
+    label: 'D+',
+    value: 'D+'
+  },
+  {
+    label: 'D',
+    value: 'D'
+  },
+  {
+    label: 'E',
+    value: 'E'
+  }
   
+ ];
+  constructor(private http: HttpClient) { }
+
   getResultByEpNumber(epNumber: string) {
     //const headers = new HttpHeaders({ "Access-Control-Allow-Origin": "*" });
 
-    return this.http.get<any>("http://localhost:8080/result/findReultByEpNum/"+ epNumber);
+    return this.http.get<any>("http://localhost:8080/result/findReultByEpNum/" + epNumber);
+  }
+
+  getbatchs() {
+    return this.http.get<any>("http://localhost:8080/result/findbatch");
+  }
+
+  getCourse(lectureId: String) {
+    return this.http.get<any>("http://localhost:8080/result/findcourse/" + lectureId);
+  }
+
+  findStudents(courseId: string, batch: string) {
+    let parameters = new HttpParams();
+    parameters = parameters.set('courseId', courseId).set('batch', batch);
+    return this.http.get<any>("http://localhost:8080/result/findStudent", { params: parameters });
   }
   _set_ngxModal_add(value: boolean) {
     this._ngxModal_add.next(value);
@@ -34,5 +99,7 @@ export class ResultService {
   get_ngxModal_edit_$(): Observable<boolean> {
     return this._ngxModal_edit.asObservable();
   }
+
+
 
 }
